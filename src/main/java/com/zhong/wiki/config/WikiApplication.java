84f0bd701d7d7115b80bbad6f4340d841041c0d9
zhong <1759397117@qq.com>
2022-09-01 16:44:38 +0800

    2-6 修改启动类的路径,添加扫描路径.md

--- src/main/java/com/zhong/wiki/config/WikiApplication.java
+++ src/main/java/com/zhong/wiki/config/WikiApplication.java
@@ -1,11 +1,14 @@
-package com.zhong.wiki;
+package com.zhong.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+//@ComponentScan({"com.zhong","com.test"})
 
+@ComponentScan("com.zhong")
 @SpringBootApplication
 public class WikiApplication {
 
