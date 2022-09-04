0edbef5b94fed431c8aafdf15d398242cebeeda8
zhong <1759397117@qq.com>
2022-09-04 22:09:18 +0800

    4-3 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    久远电子书
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+
+});
+
+</script>
