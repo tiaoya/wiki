663dde8b165ef9c48d60c7f4b4162822c737ae32
zhong <1759397117@qq.com>
2022-09-04 22:09:18 +0800

    4-3 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,9 +2,7 @@
   <a-layout id="components-layout-demo-top-side-2">
     <the-header></the-header>
       <router-view/>
-    <a-layout-footer style="text-align: center">
-        久远电子书
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
@@ -12,11 +10,13 @@
 
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 
 export default defineComponent({
   name: 'app',
   components:{
     TheHeader,
+    TheFooter
   }
 
 });
