06cf73211074d49e841d128124ad88666bd31c32
zhong <1759397117@qq.com>
2022-09-06 20:51:37 +0800

    5-9 增加电子书管理页面 新页面开发,页面,路由,菜单

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import AdminEbook from '../views/admin/admin-ebook.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -9,12 +11,18 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'AboutView',
+    component:AboutView
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/admin/ebook',
+    name: 'AdminEbook',
+    component: AdminEbook
+  },
 ]
 
 const router = createRouter({
