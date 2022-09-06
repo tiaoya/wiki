7388c114403eaab03beba654aedb1c73fe73a28d
zhong <1759397117@qq.com>
2022-09-06 22:04:03 +0800

    6-1 电子书管理表格展示数据

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -9,7 +9,7 @@
           <template #title>
                 <span>
                   <user-outlined />
-                  subnav 11111
+                  subnav 1
                 </span>
           </template>
           <a-menu-item key="1">option1</a-menu-item>
