ab18ecb25e90bc4b375484b3b00a9f2e3b309a3f
zhong <1759397117@qq.com>
2022-09-17 21:53:15 +0800

    10-6 首页显示电子书信息: 文档数,阅读数,点赞数 doc/vote/** 接口不需要登录拦截

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -30,9 +30,17 @@
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
-              <span v-for="{ type, text } in actions" :key="type">
-                <component v-bind:is="type" style="margin-right: 8px" />
-                {{ text }}
+              <span>
+                <component v-bind:is="'FileOutlined'" style="margin-right: 8px" />
+                {{ item.docCount }}
+              </span>
+              <span>
+                <component v-bind:is="'UserOutlined'" style="margin-right: 8px" />
+                {{ item.viewCount }}
+              </span>
+              <span>
+                <component v-bind:is="'LikeOutlined'" style="margin-right: 8px" />
+                {{ item.voteCount }}
               </span>
             </template>
 
@@ -162,11 +170,12 @@ export default defineComponent({
       pageSize: 3,
     };
 
-    const actions: Record<string, string>[] = [
-      { type: 'StarOutlined', text: '156' },
-      { type: 'LikeOutlined', text: '156' },
-      { type: 'MessageOutlined', text: '2' },
-    ];
+    // const actions: Record<string, string>[] = [
+    //   { type: 'StarOutlined', text: '156' },
+    //   { type: 'LikeOutlined', text: '156' },
+    //   { type: 'MessageOutlined', text: '2' },
+    // ];
+    //
     return {
       ebooks,
       // ebooks2: toRef(ebooks1,"books")
@@ -175,7 +184,7 @@ export default defineComponent({
 
       listData,
       pagination,
-      actions,
+      // actions,
       isShowWelcome,
     }
   }
